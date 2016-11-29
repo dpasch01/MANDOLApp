4bfb86b17630aa2efe92b3872bbc55ffbae724cc
dpasch01 <dpasch01@cs.ucy.ac.cy>
2016-11-29 16:52:13 +0200

    Cordova's inAppBrowser setup.

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,51 +1,12 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "com.darktalker.cordova.screenshot.screenshot",
-        "file": "plugins/com.darktalker.cordova.screenshot/www/Screenshot.js",
-        "pluginId": "com.darktalker.cordova.screenshot",
-        "merges": [
-            "navigator.screenshot"
-        ]
-    },
-    {
-        "id": "cordova-plugin-camera.Camera",
-        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
-        "pluginId": "cordova-plugin-camera",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
         "clobbers": [
-            "Camera"
-        ]
-    },
-    {
-        "id": "cordova-plugin-camera.CameraPopoverOptions",
-        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "CameraPopoverOptions"
-        ]
-    },
-    {
-        "id": "cordova-plugin-camera.camera",
-        "file": "plugins/cordova-plugin-camera/www/Camera.js",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "navigator.camera"
-        ]
-    },
-    {
-        "id": "cordova-plugin-camera.CameraPopoverHandle",
-        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "CameraPopoverHandle"
-        ]
-    },
-    {
-        "id": "cordova-plugin-themeablebrowser.themeablebrowser",
-        "file": "plugins/cordova-plugin-themeablebrowser/www/themeablebrowser.js",
-        "pluginId": "cordova-plugin-themeablebrowser",
-        "clobbers": [
-            "cordova.ThemeableBrowser"
+            "cordova.InAppBrowser.open",
+            "window.open"
         ]
     }
 ];
@@ -54,11 +15,8 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.3.0",
     "cordova-plugin-console": "1.0.5-dev",
-    "com.darktalker.cordova.screenshot": "0.1.5",
-    "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-camera": "2.3.0",
-    "cordova-plugin-crosswalk-webview": "2.1.0",
-    "cordova-plugin-themeablebrowser": "0.2.17"
+    "cordova-plugin-crosswalk-webview": "2.2.0",
+    "cordova-plugin-inappbrowser": "1.6.0-dev"
 };
 // BOTTOM OF METADATA
 });
--- www/js/index.js
+++ www/js/index.js
@@ -1,148 +1,26 @@
 
 $(document).on("deviceready", function() {
-
-  function report(){
-    navigator.screenshot.URI(function(error,res){
-      if(error){
-        console.error(error);
-      }else{
-        $(".screenshot").attr("src", res.URI);
-      }
-    }, 100);
-  };
-
-  function loadStartCallBack() {
-    console.log("Loading..");
-  }
-
-  function loadStopCallBack() {
-    console.log("Stopping CallBack..");
-    if (inAppBrowserRef != undefined) {
-      inAppBrowserRef.show();
-    }
-  }
-
-  function loadErrorCallBack(params) {
-    var scriptErrorMesssage = "alert('Sorry we cannot open that page. Message from the server is : " + params.message + "');"
-    inAppBrowserRef.executeScript({ code: scriptErrorMesssage }, executeScriptCallBack);
-    inAppBrowserRef.close();
-    inAppBrowserRef = undefined;
-  }
-
-  function executeScriptCallBack(params) {
-    if (params[0] == null) {
-      $('#browser-btn').css("background-color","#FF0000");
-    }
-  }
-
-  $("#report-btn").on("touchend", report);
-  $("#browser-btn").on("click", function(e){
-    var url = "http://www.google.com/";
-    var target = "_blank";
-    var options = {
-      statusbar: {
-        color: '#ffffffff'
-      },
-      toolbar: {
-        height: 44,
-        color: '#f0f0f0ff'
-      },
-      title: {
-        color: '#003264ff',
-        showPageTitle: true
-      },
-      backButton: {
-        image: 'back',
-        imagePressed: 'back_pressed',
-        align: 'left',
-        event: 'backPressed'
-      },
-      forwardButton: {
-        image: 'forward',
-        imagePressed: 'forward_pressed',
-        align: 'left',
-        event: 'forwardPressed'
-      },
-      closeButton: {
-        image: 'close',
-        imagePressed: 'close_pressed',
-        align: 'left',
-        event: 'closePressed'
-      },
-      customButtons: [
-        {
-            image: 'share',
-            imagePressed: 'share_pressed',
-            align: 'right',
-            event: 'sharePressed'
-        }
-      ],
-      menu: {
-        image: 'menu',
-        imagePressed: 'menu_pressed',
-        title: 'Test',
-        cancel: 'Cancel',
-        align: 'right',
-        items: [
-            {
-                event: 'helloPressed',
-                label: 'Hello World!'
-            },
-            {
-                event: 'testPressed',
-                label: 'Test!'
-            }
-        ]
-      }
-    };
-
-    inAppBrowserRef = cordova.ThemeableBrowser.open(url, target, options)
-      .addEventListener('backPressed', function(e) {
-        console.log('back pressed');
-      }).addEventListener('helloPressed', function(e) {
-        console.log('hello pressed');
-      }).addEventListener('sharePressed', function(e) {
-        console.log(e.url);
-      }).addEventListener(cordova.ThemeableBrowser.EVT_ERR, function(e) {
-        console.error(e.message);
-      }).addEventListener(cordova.ThemeableBrowser.EVT_WRN, function(e) {
-        console.error(e.message);
-      });
-  });
-
-});
-
-$(function(){
-
   var selection="";
 
-  function getSelection(){
-    if (window.getSelection) {
-      selection = window.getSelection();
-    } else if (document.selection) {
-      selection = document.selection.createRange();
-    }
-  }
+  $("#browser-btn").on("click", function(e){
+    var inAppBrowser = cordova.InAppBrowser.open('https://en.wikipedia.org/wiki/Main_Page', '_blank', 'location=no, toolbar=no');
+    inAppBrowser.addEventListener('loadstop', function() {
+      inAppBrowser.executeScript({file:"https://3932f31a.ngrok.io/mandolapp/www/js/report.js"});
+      inAppBrowser.insertCSS({file:"https://3932f31a.ngrok.io/mandolapp/www/css/report.css"});
+      inAppBrowser.executeScript({code: "localStorage.setItem('annotatedText', '')"});
+      var listenForAnnotation = setInterval(function(){
+        inAppBrowser.executeScript({ code: "localStorage.getItem('annotatedText')" }, function(annotated) {
+          if(annotated!=''){
+            console.log(annotated);
+            inAppBrowser.executeScript({code: "localStorage.setItem('annotatedText', '')"});
+            inAppBrowser.hide();
+          }
+        });
+      }, 500);
+    });
 
-  $("body").bind( "taphold", tapholdHandler );
-  function tapholdHandler(e){
-    selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
-  }
-
-  $("body").swipe({
-    swipe: function(e, direction, distance, duration, fingerCount, fingerData) {
-      getSelection();
-      selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
-    },
-    threshold:0
+    inAppBrowser.addEventListener('exit', function() {
+        clearInterval(listenForAnnotation);
+    });
   });
-
-  $("body").swipe({
-    hold: function(e, target) {
-      getSelection();
-      selection.toString() !== '' && alert('"' + selection.toString() + '" was selected at ' + e.pageX + '/' + e.pageY);
-    },
-    threshold:50
-  });
-
 });
